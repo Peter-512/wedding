4664ae8eed64ed2973df5a9e868b9d78cf98bec8
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2024-08-25 10:54:43 +0200

    feat: basic contact email template and server function to send an email

---
+++ src/lib/components/email/Contact.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Hr, Html, Text } from 'svelte-email';
+
+	export let name: string;
+	export let message: string;
+	export let sender: string;
+</script>
+
+<Html lang="en">
+	<Text>
+		Hello from {name} 👋🏻!
+	</Text>
+	{#if message}
+		<Hr />
+		<Text>
+			{message}
+		</Text>
+	{/if}
+	<Hr />
+	<Text>
+		Have a great day!
+		{sender}
+	</Text>
+</Html>
