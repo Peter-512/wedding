33f0a134294c095b3552c92b85a87bdeb8fcacd4
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2024-09-21 19:07:08 +0200

    feat: cleaner lang switcher with flags and text

--- src/lib/components/LanguageSwitcher.svelte
+++ src/lib/components/LanguageSwitcher.svelte
@@ -1,23 +1,35 @@
 <script>
 	import { availableLanguageTags } from '$lib/paraglide/runtime.js';
 	import { Content, Item, DropdownMenu, Trigger } from '$lib/components/ui/dropdown-menu/index';
-	import { Languages } from 'lucide-svelte';
 	import { i18n } from '$lib/i18n';
+	import { languageTag } from '$lib/paraglide/runtime.js';
 	import { page } from '$app/stores';
 	import * as m from '$lib/paraglide/messages.js';
 
 	const currentPathWithoutLanguage = $derived.by(() => i18n.route($page.url.pathname));
+
+	const flags = {
+		en: '🇺🇸',
+		de: '🇦🇹',
+		nl: '🇧🇪'
+	};
+
+	const langs = {
+		en: 'English',
+		de: 'Deutsch',
+		nl: 'Nederlands',
+	}
 </script>
 
 <DropdownMenu preventScroll={false}>
-	<Trigger
-		><Languages />
+	<Trigger>
+		{flags[languageTag()]}
 		<span class="sr-only">{m.language()}</span>
 	</Trigger>
 	<Content>
 		{#each availableLanguageTags as lang}
 			<Item href={currentPathWithoutLanguage} hreflang={lang}>
-				{lang}
+				{flags[lang]} {langs[lang]}
 			</Item>
 		{/each}
 	</Content>
