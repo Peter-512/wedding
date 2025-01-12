d959204da7164875873f2c0ca38b416594d93192
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2025-01-12 13:41:33 +0100

    translations layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,10 +35,10 @@
 				<MediaQuery size="isAboveMD">
 					{#snippet match()}
 						<Button class="text-lg" variant="link" href="/travel-information">
-							Travel information
+							{m.travel_information()}
 						</Button>
-						<Button class="text-lg" variant="link" href="/timeline">Timeline</Button>
-						<Button class="text-lg" variant="link" href="/favorite-spots">Favorite spots</Button>
+						<Button class="text-lg" variant="link" href="/timeline">{m.timeline()}</Button>
+						<Button class="text-lg" variant="link" href="/favorite-spots">{m.fave_spots()}</Button>
 						<Button class="text-lg" variant="link" href="/#rsvp">
 							{m.rsvp()}
 						</Button>
@@ -49,9 +49,9 @@
 								<MenuIcon />
 							</Trigger>
 							<DropdownMenuContent>
-								<Item><a href="/travel-information">Travel information</a></Item>
-								<Item><a href="/timeline">Timeline</a></Item>
-								<Item><a href="/favorite-spots">Favorite spots</a></Item>
+								<Item><a href="/travel-information">{m.travel_information()}</a></Item>
+								<Item><a href="/timeline">{m.timeline()}</a></Item>
+								<Item><a href="/favorite-spots">{m.fave_spots()}</a></Item>
 								<Item><a href="/#rsvp">{m.rsvp()}</a></Item>
 							</DropdownMenuContent>
 						</DropdownMenu>
@@ -67,7 +67,7 @@
 			class="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6"
 		>
 			<p class="text-xs text-muted-foreground">
-				© 2024 Charlotte &amp; Peter. All rights reserved.
+				© 2024 Charlotte &amp; Peter. {m.all_rights_reserved()}
 			</p>
 		</footer>
 	</div>
