9d8af11d47810598c23a78fec96e18b038601d12
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2025-03-12 19:23:45 +0100

    add carousel component with autoplay and timeline layout adjustments

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { cls, Timeline, TimelineEvent } from 'svelte-ux';
 	import * as m from '$lib/paraglide/messages';
+	import Autoplay from 'embla-carousel-autoplay';
 	import {
 		mdiSilverwareForkKnife,
 		mdiPartyPopper,
@@ -8,6 +9,8 @@
 		mdiGlassCocktail,
 		mdiDoorOpen
 	} from '@mdi/js';
+	import { Carousel, Content, Item, Previous, Next } from '$lib/components/ui/carousel/index.js';
+	import CarouselItem from './Carouseltem.svelte';
 
 	type TimelineEntry = {
 		time: `${number}:${number}`;
@@ -50,24 +53,54 @@
 	];
 </script>
 
-<Timeline vertical snapPoint>
-	{#each data as { icon, title, time, description }, i}
-		<TimelineEvent
-			{icon}
-			start={i % 2 === 0}
-			end={i % 2 !== 0}
-			classes={{
-				root: cls(i % 2 === 0 && 'text-end', 'not-prose'),
-				icon: 'size-5'
-			}}
-		>
-			<div class="mx-2 mb-10 mt-0.5">
-				<time class="font-mono italic">{time}</time>
-				<div class="text-lg font-black">{title}</div>
-				<div class="text-sm text-muted-foreground">
-					{description}
+<div
+	class="container flex flex-col items-center gap-4 px-16 md:flex-row md:items-center md:justify-center md:gap-8"
+>
+	<Timeline vertical snapPoint>
+		{#each data as { icon, title, time, description }, i}
+			<TimelineEvent
+				{icon}
+				start={i % 2 === 0}
+				end={i % 2 !== 0}
+				classes={{
+					root: cls(i % 2 === 0 && 'text-end', 'not-prose'),
+					icon: 'size-5'
+				}}
+			>
+				<div class="mx-2 mb-10 mt-0.5">
+					<time class="font-mono italic">{time}</time>
+					<div class="text-lg font-black">{title}</div>
+					<div class="text-sm text-muted-foreground">
+						{description}
+					</div>
 				</div>
-			</div>
-		</TimelineEvent>
-	{/each}
-</Timeline>
+			</TimelineEvent>
+		{/each}
+	</Timeline>
+	<Carousel
+		plugins={[
+			Autoplay({
+				delay: 3500
+			})
+		]}
+		opts={{ loop: true }}
+		class="w-full max-w-sm"
+	>
+		<Previous />
+		<Content class="not-prose -ml-1">
+			<CarouselItem>
+				<enhanced:img class="rounded-lg" src="../../lib/images/black-and-white.jpg" />
+			</CarouselItem>
+			<CarouselItem>
+				<enhanced:img class="rounded-lg" src="../../lib/images/cool.jpg" />
+			</CarouselItem>
+			<CarouselItem>
+				<enhanced:img class="rounded-lg" src="../../lib/images/sweet.jpg" />
+			</CarouselItem>
+			<CarouselItem>
+				<enhanced:img class="rounded-lg" src="../../lib/images/hands.jpg" />
+			</CarouselItem>
+		</Content>
+		<Next />
+	</Carousel>
+</div>
